f7e55ddb49f65837dbaf9c58f65542b453467254
newchild <fabianbrand98@gmx.de>
2015-09-19 01:43:50 +0200

    Evade soon™ Kappa

---
+++ EvadeBuddy/EvadeBuddy/Evade.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EloBuddy;
+using SharpDX;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvadeBuddy
+{
+	class Evade
+	{
+		private enum Mode{
+			blocking,
+			evading
+		}
+
+
+		private float _Distance(Vector3 VecOne, Vector3 VecTwo)
+		{
+			return (VecTwo - VecOne).Length() > 0.0f ? (VecTwo - VecOne).Length() : (VecOne - VecTwo).Length();
+		}
+
+		private float _Dot(Vector3 VectorOne, Vector3 VectorTwo)
+		{
+			return VectorOne.X * VectorTwo.X + VectorOne.Y * VectorTwo.Y + VectorOne.Z * VectorTwo.Z;
+		}
+
+		private bool _PosInSpell(Obj_AI_Base target, GameObjectProcessSpellCastEventArgs spell)
+		{
+			if (_Dot(spell.End - spell.Start, target.Position - spell.Start) < 0)
+				return false;
+			if (_Dot(spell.End - spell.Start, target.Position - spell.Start) > _Distance(spell.Start, spell.End) * _Distance(spell.Start, spell.End))
+				return false;
+			return true;
+		}
+
+		private void _Evade_OnSpellProc(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+		{
+			EvadableSpell temp = new EvadableSpell(args.SData, EvadableSpell.Type.Line);
+			Chat.Print(_PosInSpell(sender, args).ToString() + " <- IsInSpell ", temp.getWidth().ToString() + " <- Width");
+		}
+
+
+		private Dictionary<Obj_AI_Base, Mode> _Objects = new Dictionary<Obj_AI_Base, Mode>();
+		public Evade(Obj_AI_Base Evading)
+		{
+			if (Evading.IsMe || Evading == ObjectManager.Player.Pet)
+			{
+				_Objects.Add(Evading, Mode.evading);
+				Obj_AI_Base.OnProcessSpellCast += _Evade_OnSpellProc;
+			}	
+				
+		}
+
+		
+	}
+}
---
+++ EvadeBuddy/EvadeBuddy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EloBuddy;
+using EloBuddy.SDK.Menu;
+using System.Threading.Tasks;
+
+namespace EvadeBuddy
+{
+	class Program
+	{
+		private static Evade _EvadeInst;
+		static void Main(string[] args)
+		{
+			Game.OnLoad += Game_OnLoad;
+		}
+
+		static void Game_OnLoad(EventArgs args)
+		{
+			_EvadeInst = new Evade(ObjectManager.Player);
+			Chat.Print("loaded");
+		}
+	}
+}
