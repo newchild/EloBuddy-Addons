dcf2a551abc50c3f3d9598c2f84dfad822e351c1
newchild <fabianbrand98@gmx.de>
2015-09-19 14:16:40 +0200

    Added SKinhack

--- NewchildUtils/NewchildUtils/Program.cs
+++ NewchildUtils/NewchildUtils/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using EloBuddy;
 using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
 using EloBuddy.SDK.Events;
 using System.Threading.Tasks;
 
@@ -11,6 +13,8 @@ namespace NewchildUtils
 {
 	class Program
 	{
+		private static Dictionary<AIHeroClient, Slider> _SkinVals = new Dictionary<AIHeroClient, Slider>();
+
 		enum Keys
 		{
 			LMB = 0x01,
@@ -25,9 +29,33 @@ namespace NewchildUtils
 			Numpad8,
 			Numpad9
 		}
+
+		private static Menu _Menu;
+
 		static void Main(string[] args)
 		{
 			Game.OnWndProc += Game_OnWndProc;
+			Loading.OnLoadingComplete+=Loading_OnLoadingComplete;
+			
+		}
+
+		private static void Loading_OnLoadingComplete(EventArgs args)
+		{
+			_Menu = MainMenu.AddMenu("SkinHack", "skinhackMenu");
+			foreach (var hero in HeroManager.AllHeroes)
+			{
+				_SkinVals.Add(hero, _Menu.Add(hero.BaseSkinName, new Slider("Skin ID", 0, 0, 9)));
+				_SkinVals[hero].OnValueChange+=Program_OnValueChange;
+			}
+
+		}
+
+		private static void Program_OnValueChange(ValueBase<int> sender, ValueBase<int>.ValueChangeArgs args)
+		{
+			var hero = ObjectManager.Get<AIHeroClient>().Where(x => x.BaseSkinName == sender.DisplayName).FirstOrDefault();
+			if (hero == null)
+				return;
+			hero.SetSkinId(args.NewValue);
 		}
 
 		static void Game_OnWndProc(WndEventArgs args)
