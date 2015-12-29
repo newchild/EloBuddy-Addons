828b79da3b7279b2c02f7053c74ce4e88b9373d4
newchild <fabianbrand98@gmx.de>
2015-12-29 02:09:30 +0100

    EB Utilities are definetily not a secret project

---
+++ EB Utilities/Emulator/Emulator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulator
+{
+    public class Emulator
+    {
+	    private Registers _registers;
+	    private Clock _clock;
+	    public Emulator()
+	    {
+		    _registers = new Registers();
+			_clock = new Clock();
+	    }
+    }
+}
